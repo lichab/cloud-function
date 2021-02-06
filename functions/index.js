f6016c33e1ac27df3a07bffc337e1eb02e7bf3ca
Lisandro <lisandro.bertoli@gmail.com>
2021-02-06 16:24:20 +0100

    add first cloud-function example

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,8 @@
-const functions = require("firebase-functions");
+const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// http request 1
+exports.randomNumber = functions.https.onRequest((req, res) => {
+  const number = Math.round(Math.random() * 100);
+
+  res.send(number.toString());
+});

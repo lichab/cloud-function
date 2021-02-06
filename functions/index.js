d6a8669c9bb86c57887afeff6aed8185dae11602
Lisandro <lisandro.bertoli@gmail.com>
2021-02-06 18:22:22 +0100

    add real implementation for auth triggers

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,19 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp();
 
 // auth trigger for new user
 exports.onNewUser = functions.auth.user().onCreate(user => {
-  console.log('user created', user.email, user.uid);
+  return admin.firestore().collection('users').doc(user.uid).set({
+    email: user.email,
+    upVotedOn: [],
+  });
 });
 
 // auth trigger for deleted user
 exports.onUserDeleted = functions.auth.user().onDelete(user => {
-  console.log('user deleted', user.email, user.uid);
+  const document = admin.firestore().collection('users').doc(user.uid);
+
+  return document.delete();
 });
